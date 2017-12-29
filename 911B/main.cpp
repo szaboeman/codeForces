252b8fab8d755ebf2a52fdf9e30104c29e74dec8
szaboeman <szab.eman@gmail.com>
2017-12-29 21:44:58 +0100

    day3

--- 911B/main.cpp
+++ 911B/main.cpp
@@ -1,44 +1,22 @@
-/*#include <iostream>
-#include <cmath>
-using namespace std;
-
-int main() {
-    double n,a,b;
-    cin>>n>>a>>b;
-    if (b<a) {
-        int sv=a;
-        a=b;
-        b=sv;
-    }
-    double atlag=(double)(a+b)/n;
-    cout<<atlag<<endl;
-    double p1=(a/atlag<1)?1:round(a/atlag);
-    double p2=n-p1;
-
-    cout<<a<<" "<<b<<endl;
-    cout<<p1<<" "<<p2<<endl;
-    cout<<(int)(a/p1)<<" "<<(int)(b/p2)<<endl;
-
-    int moda=a-p1*(int)(a/p1);
-    int modb=b-p2*(int)(b/p2);
-    cout<<moda<<" "<<modb<<endl;
-    if (moda>0) moda=0;
-    else if (moda<0) moda=-1;
-    if (modb<0) modb=-1;
-    else if (modb>0) modb=0;
-    cout<<moda<<" "<<modb<<endl;
-    cout<<min((int)(a/p1)+moda,(int)(b/p2)+modb);
-    return 0;
-
-}
-*/
-
 #include <iostream>
 #include <cmath>
 using namespace std;
 
+int getMin(int a, int b, int p1, int p2) {
+
+    if (p2==0) {
+        return 0;
+    } else {
+        int moda=a-p1*(int)(a/p1);
+        int modb=b-p2*(int)(b/p2);
+
+        if (moda>0) moda=0; else if (moda<0) moda=-1;
+        if (modb<0) modb=-1; else if (modb>0) modb=0;
+        return min((int)(a/p1)+moda,(int)(b/p2)+modb);
+    }
+}
+
 int main() {
-    while(true) {
     double n,a,b;
     cin>>n>>a>>b;
     if (a<b) {
@@ -46,26 +24,12 @@ int main() {
         a=b;
         b=sv;
     }
-    cout<<a<<" "<<b<<endl;
-    double p2=round((n/(a+b))*b);
-    double p1=(n-p2);
-    cout<<(p1)<<" "<<(p2)<<endl;
-    cout<<(a/p1)<<" "<<(b/p2)<<endl;
 
+    int v1=getMin(a,b,n-ceil((n/(a+b))*b),ceil((n/(a+b))*b));
+    int v2=getMin(a,b,n-floor((n/(a+b))*b),floor((n/(a+b))*b));
 
-    int moda=a-p1*(int)(a/p1);
-    int modb=b-p2*(int)(b/p2);
+    cout<<max(v1,v2);
 
-    cout<<moda<<" "<<modb<<endl;
-    if (moda>0) moda=0;
-    else if (moda<0) moda=-1;
-    if (modb<0) modb=-1;
-    else if (modb>0) modb=0;
-    cout<<moda<<" "<<modb<<endl;
-    cout<<min((int)(a/p1)+moda,(int)(b/p2)+modb)<<endl;
-
-
-    }
     return 0;
 
 }
